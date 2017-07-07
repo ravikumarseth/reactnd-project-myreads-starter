e1fafacb8188540e361c5d719e82dc260f5a2839
Ravi Kumar Seth <ravi.kseth.che12@iitbhu.ac.in>
2017-07-07 19:47:53 +0530

    Change ListItem to stateless functional component

--- src/ListItem.js
+++ src/ListItem.js
@@ -1,15 +1,14 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
-class ListItem extends React.Component {
-  render() {
+const ListItem = props => {
     return (
       <li>
         <div className='book'>
           <div className="book-top">
             <div className="book-shelf-changer">
-              <select value={this.props.getShelf(this.props.book)} onChange={
-                e => this.props.updateSection(this.props.book, e.target.value)}>
+              <select value={props.getShelf(props.book)} onChange={
+                e => props.updateSection(props.book, e.target.value)}>
                 <option value="select" disabled>Move to...</option>
                 <option value="currentlyReading">Currently Reading</option>
                 <option value="wantToRead">Want to Read</option>
@@ -21,24 +20,23 @@ class ListItem extends React.Component {
             <div className="book-cover" style={{
               width: 128,
               height: 193,
-              backgroundImage: `url(${this.props.book.imageLinks.thumbnail})`
+              backgroundImage: `url(${props.book.imageLinks.thumbnail})`
             }}></div>
           </div>
-          <Link to={'/books/' + this.props.book.id} style={{
+          <Link to={'/books/' + props.book.id} style={{
             textDecoration: "none",
             color: "black",
             textAlign: "center"
           }}>
-            <div className="book-title">{this.props.book.title}</div>
-            <ul className="book-authors">{this.props.book.authors &&
-              (this.props.book.authors.map((author, index) => (
+            <div className="book-title">{props.book.title}</div>
+            <ul className="book-authors">{props.book.authors &&
+              (props.book.authors.map((author, index) => (
                 <li key={index}>{author}</li>
               )))}</ul>
           </Link>
         </div>
       </li>
     )
-  }
 }
 
 export default ListItem
